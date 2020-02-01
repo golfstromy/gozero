37e3718bbed603ac00b331067b38eb5f7016ae87
David Forster <dfo@ivu.de>
2020-02-02 00:00:23 +0100

    saved frequently used UI constants

---
+++ go_zero/lib/screens/mvp_route/ui/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class GoZeroColors {
+  static const Color green = Color.fromRGBO(73, 162, 104, 1.0);
+  static const Color yellow = Color.fromRGBO(235, 169, 69, 1.0);
+//ggf. MaterialColor erstellen
+}
