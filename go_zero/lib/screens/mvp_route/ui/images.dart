69dd871aa37b86afe3f23ce9016228a22c2b03b4
David Forster <dfo@ivu.de>
2020-02-02 04:06:58 +0100

    simplified access to logo and icons

--- go_zero/lib/screens/mvp_route/ui/images.dart
+++ go_zero/lib/screens/mvp_route/ui/images.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 
-const String LOGOWITHOUTARROWS = "assets/logoWithoutArrows.png";
-const String LEFTARROW = "assets/arrows/leftArrow.png";
-const String RIGHTARROW = "assets/arrows/rightArrow.png";
+const String LOGOWITHOUTARROWS = "assets/logo/logoWA4x.png";
+const String TOPARROW = "assets/arrows/topArrow.png";
+const String BOTTOMARROW = "assets/arrows/bottomArrowSmall.png";
 
-Widget LOGOWIDGET = Column(
-  children: <Widget>[
-    Padding(
-      padding: EdgeInsets.only(bottom: 6),
-      child: Image.asset(RIGHTARROW),
-    ),
-    Image.asset(LOGOWITHOUTARROWS),
-    Padding(
-      padding: EdgeInsets.only(top: 6),
-      child: Image.asset(LEFTARROW),
-    ),
-  ],
-);
+const String GLOBEICON = "assets/icons/globe4x.png";
+
+abstract class GoZeroIcons {
+  static final Widget globe =  Image.asset(GLOBEICON, width: 16, height: 16);
+}
+
+Widget LOGOWIDGET([double multiplier = 1]) {
+  return Column(
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.only(bottom: 6),
+        child: Image.asset(TOPARROW,
+            width: multiplier * 20.75, height: multiplier * 4.75),
+      ),
+      Image.asset(LOGOWITHOUTARROWS,
+          width: multiplier * 75.75, height: multiplier * 12),
+      Padding(
+        padding: EdgeInsets.only(top: 6),
+        child: Image.asset(BOTTOMARROW,
+            width: multiplier * 20.75, height: multiplier * 4.75),
+      ),
+    ],
+  );
+}
