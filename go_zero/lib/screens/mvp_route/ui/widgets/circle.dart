6a82dad38375fbc57f82d3eef76f56cbda30ee9a
CodingDavid8 <codingdavid@gmx.de>
2020-02-02 17:28:28 +0100

    implemented method for standartized text in circles

--- go_zero/lib/screens/mvp_route/ui/widgets/circle.dart
+++ go_zero/lib/screens/mvp_route/ui/widgets/circle.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../colors.dart';
+import '../textstyle.dart';
+
 const double DEFAULTWIDTH = 264;
 
 Widget bigCircle(
-    {double width = DEFAULTWIDTH,
-    double height = DEFAULTWIDTH,
-    BuildContext context,
+    {double size = DEFAULTWIDTH,
     Widget child = const SizedBox(),
     double thickness = DEFAULTWIDTH / 132}) {
   return Container(
-      width: width,
-      height: height,
+      width: size,
+      height: size,
       child: Align(alignment: Alignment.center, child: child),
       decoration: BoxDecoration(
           shape: BoxShape.circle,
           border: Border.all(
-              color: Color.fromRGBO(235, 235, 235, 1.0), width: thickness))
-      //borderRadius: BorderRadius.all(Radius.circular(40.0))),
-      );
+              color: GoZeroColors.circleGrey, width: thickness)));
+}
+
+Widget circleWithText(String blacktext, String greenText,
+    {bool blackFullStop = true,
+    double size = DEFAULTWIDTH,
+    double thickness = DEFAULTWIDTH / 132}) {
+  return bigCircle(
+      size: size,
+      thickness: thickness,
+      child: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: GoZeroTextStyles.bold(),
+            children: <TextSpan>[
+              TextSpan(text: blacktext),
+              TextSpan(
+                  text: greenText,
+                  style: GoZeroTextStyles.semibold(color: GoZeroColors.green)),
+              TextSpan(text: blackFullStop ? "." : ""),
+            ],
+          )));
 }
