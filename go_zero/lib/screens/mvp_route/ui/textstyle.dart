acddf285981faf33aeb930fb94c4299579ef5274
CodingDavid8 <codingdavid@gmx.de>
2020-02-02 17:27:43 +0100

    changed signatures, generalized constants, added medium style

--- go_zero/lib/screens/mvp_route/ui/textstyle.dart
+++ go_zero/lib/screens/mvp_route/ui/textstyle.dart
@@ -1,45 +1,50 @@
 import 'package:flutter/material.dart';
 
 abstract class GoZeroTextStyles {
+  static const String FONTFAMILY = "Montserrat";
 
-static const double MULTIPLIER = 1;
-static const double DEFAULTBOLDSIZE = MULTIPLIER * 24;
-static const double DEFAULTREGULARSIZE = MULTIPLIER * 9;
+  static const double MULTIPLIER = 1;
+  static const double DEFAULTBOLDSIZE = MULTIPLIER * 24;
+  static const double DEFAULTREGULARSIZE = MULTIPLIER * 9;
+  //static const double RECOMMENDEDSPACING = 0.16;
 
   static TextStyle semibold(
       {Color color = Colors.black, double fontSize = DEFAULTBOLDSIZE}) {
     return TextStyle(
         color: color,
-        fontFamily: "Montserrat",
+        fontFamily: FONTFAMILY,
         fontSize: fontSize,
         fontWeight: FontWeight.w600);
   }
 
-  static TextStyle bold({Color color = Colors.black, double fontSize = DEFAULTBOLDSIZE}) {
+  static TextStyle bold(
+      {Color color = Colors.black, double fontSize = DEFAULTBOLDSIZE}) {
     return TextStyle(
         color: color,
-        fontFamily: "Montserrat",
+        fontFamily: FONTFAMILY,
         fontSize: fontSize,
         fontWeight: FontWeight.bold);
   }
 
-
-  static TextStyle regular([double fontSize = DEFAULTREGULARSIZE, Color color = Colors.black]) {
+  static TextStyle regular(double fontSize,
+      {Color color = Colors.black, double letterSpacingFactor = 0, bool italic = false}) {
     return TextStyle(
       color: color,
-      fontFamily: "Montserrat",
+      fontFamily: FONTFAMILY,
       fontSize: fontSize,
+      fontStyle: italic ? FontStyle.italic : null,
+      letterSpacing: letterSpacingFactor * fontSize,
     );
   }
 
-
-static TextStyle regularLetterSpaced({Color color = Colors.black, double fontSize = DEFAULTREGULARSIZE, double letterSpacing = 0.16*DEFAULTREGULARSIZE}) {
+  static TextStyle medium(double fontSize,
+      {Color color = Colors.black, double letterSpacingFactor = 0}) {
     return TextStyle(
       color: color,
-      fontFamily: "Montserrat",
+      fontFamily: FONTFAMILY,
+      fontWeight: FontWeight.w500,
       fontSize: fontSize,
-      letterSpacing: letterSpacing,
+      letterSpacing: letterSpacingFactor * fontSize,
     );
-  }  
+  }
 }
-
