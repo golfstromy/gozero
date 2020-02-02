5da2ad0f8142ab9cdf4b0d7cb3b8989b9839646c
CodingDavid8 <codingdavid@gmx.de>
2020-02-02 21:28:24 +0100

    reformatted code

--- go_zero/lib/screens/mvp_route/ui/textstyle.dart
+++ go_zero/lib/screens/mvp_route/ui/textstyle.dart
@@ -8,8 +8,7 @@ abstract class GoZeroTextStyles {
   static const double DEFAULTREGULARSIZE = MULTIPLIER * 9;
   //static const double RECOMMENDEDSPACING = 0.16;
 
-  static TextStyle semibold(
-      {Color color = Colors.black, double fontSize = DEFAULTBOLDSIZE}) {
+  static TextStyle semibold(double fontSize, {Color color = Colors.black}) {
     return TextStyle(
         color: color,
         fontFamily: FONTFAMILY,
@@ -27,7 +26,9 @@ abstract class GoZeroTextStyles {
   }
 
   static TextStyle regular(double fontSize,
-      {Color color = Colors.black, double letterSpacingFactor = 0, bool italic = false}) {
+      {Color color = Colors.black,
+      double letterSpacingFactor = 0,
+      bool italic = false}) {
     return TextStyle(
       color: color,
       fontFamily: FONTFAMILY,
