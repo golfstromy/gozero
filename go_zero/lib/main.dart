cf77d8118eb20d3bc7e2efd357efb0d52d6c02ce
David Forster <dfo@ivu.de>
2020-02-02 04:05:20 +0100

    added new routes

--- go_zero/lib/main.dart
+++ go_zero/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_zero/screens/mvp_route/screen1.dart';
 import 'package:go_zero/screens/mvp_route/screen2.dart';
+import 'package:go_zero/screens/mvp_route/screen4.dart';
+
+import 'screens/mvp_route/screen3.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +22,8 @@ class MyApp extends StatelessWidget {
       routes: {
         '/init': (context) => InitScreen(),
         '/screen2': (context) => ChallengeScreen(),
+        '/screen3': (context) => HowItWorksScreen(),
+        '/screen4': (context) => CalculateIntroScreen()
       },
     );
   }
