a99757838572735b36ecbd5298aaf150a318cc70
CodingDavid8 <codingdavid@gmx.de>
2020-02-02 17:26:35 +0100

    added circleGrey color constant

--- go_zero/lib/screens/mvp_route/ui/colors.dart
+++ go_zero/lib/screens/mvp_route/ui/colors.dart
@@ -6,5 +6,6 @@ abstract class GoZeroColors {
   static const Color green = Color.fromRGBO(73, 162, 104, 1.0);
   static const Color yellow = Color.fromRGBO(235, 169, 69, 1.0);
   static const Color grey = Color.fromRGBO(192, 191, 191, 1.0);
+  static const Color circleGrey = Color.fromRGBO(235, 235, 235, 1.0);
 //ggf. MaterialColor erstellen
 }
