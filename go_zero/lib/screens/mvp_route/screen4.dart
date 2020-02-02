274e9fed899292b2c89fee20422bba4da035bc7c
CodingDavid8 <codingdavid@gmx.de>
2020-02-02 21:28:15 +0100

    used template for first introscreen

--- go_zero/lib/screens/mvp_route/screen4.dart
+++ go_zero/lib/screens/mvp_route/screen4.dart
@@ -1,34 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:go_zero/screens/mvp_route/ui/widgets/pressAnywhereLabel.dart';
 
-import 'ui/colors.dart';
+import 'ui/customScreenTemplate/introscreen.dart';
 import 'ui/images.dart';
-import 'ui/widgets/circle.dart';
-import 'ui/widgets/progressbar.dart';
 
 const int index = 2;
 
 class CalculateIntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: GestureDetector(
-      onTap: () => Navigator.pushNamed(context, "/screen5"),
-      child: Stack(
-        children: <Widget>[
-          Container(color: GoZeroColors.background),
-          Align(
-            alignment: Alignment.topLeft,
-            child: progressbar(index, context),
-          ),
-          Container(
-              child: Align(
-            alignment: Alignment.center,
-            child: bigCircle(child: Image.asset(CALCULATEFOOTPRINT)),
-          )),
-          pressAnywhere(context)
-        ],
-      ),
-    ));
+    return introscreen(context, 1, CALCULATEFOOTPRINT,
+        "Calculate your\npersonal CO2 footprint", index, "/screen5");
   }
 }
