0f0527db5d161913b27bff297a1ad2ec12e950f4
CodingDavid8 <codingdavid@gmx.de>
2020-02-02 20:30:30 +0100

    reformatted code

--- go_zero/lib/screens/mvp_route/ui/widgets/globe_bulletpoint.dart
+++ go_zero/lib/screens/mvp_route/ui/widgets/globe_bulletpoint.dart
@@ -8,20 +8,19 @@ const double _TEXTSIZE = 14;
 
 Widget bulletPoint(BuildContext context, List<TextSpan> infotext) {
   return Padding(
-      padding: EdgeInsets.only(bottom: 0.023 * getScreenHeight(context), left: 0.037 * getScreenWidth(context)),
+      padding: EdgeInsets.only(
+          bottom: 0.023 * getScreenHeight(context),
+          left: 0.037 * getScreenWidth(context)),
       child: Row(
-        //mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           GoZeroIcons.globe,
           Padding(
-              padding: EdgeInsets.only(
-                  left: //GAPMULTIPLIER *
-                      0.017 * getScreenWidth(context)),
+              padding: EdgeInsets.only(left: 0.017 * getScreenWidth(context)),
               child: RichText(
                   text: TextSpan(
-                      style: GoZeroTextStyles.regular(_TEXTSIZE), children: infotext)))
+                      style: GoZeroTextStyles.regular(_TEXTSIZE),
+                      children: infotext)))
         ],
       ));
 }
-//TODO: Generalize generation of bulletpoints, z.B. map String text:String fontstyle, "Global" : "regular" // see ordered_bulletpoint
