cbf2fccd45f1a24a43215bef57803609f1f67c1f
CodingDavid8 <codingdavid@gmx.de>
2020-02-02 20:29:56 +0100

    added asset

--- go_zero/lib/screens/mvp_route/ui/images.dart
+++ go_zero/lib/screens/mvp_route/ui/images.dart
@@ -4,27 +4,13 @@ const String LOGOWITHOUTARROWS = "assets/logo/logoWA4x.png";
 const String TOPARROW = "assets/arrows/topArrow.png";
 const String BOTTOMARROW = "assets/arrows/bottomArrowSmall.png";
 
+const String CALCULATEFOOTPRINT = "assets/calculateFootprint.png";
+const String OFFSETFOOTPRINT = "assets/offsetFootprint.png";
+const String SHARE = "assets/share.png";
+
 const String GLOBEICON = "assets/icons/globe4x.png";
 
 abstract class GoZeroIcons {
-  static final Widget globe =  Image.asset(GLOBEICON, width: 16, height: 16);
+  static final Widget globe = Image.asset(GLOBEICON, width: 16, height: 16);
 }
 
-Widget LOGOWIDGET([double multiplier = 1]) {
-  return Column(
-    children: <Widget>[
-      Padding(
-        padding: EdgeInsets.only(bottom: 6),
-        child: Image.asset(TOPARROW,
-            width: multiplier * 20.75, height: multiplier * 4.75),
-      ),
-      Image.asset(LOGOWITHOUTARROWS,
-          width: multiplier * 75.75, height: multiplier * 12),
-      Padding(
-        padding: EdgeInsets.only(top: 6),
-        child: Image.asset(BOTTOMARROW,
-            width: multiplier * 20.75, height: multiplier * 4.75),
-      ),
-    ],
-  );
-}
