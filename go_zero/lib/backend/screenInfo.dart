bcc073040dcdbb7750b583cf55855366b51aba58
CodingDavid8 <codingdavid@gmx.de>
2020-02-03 11:21:40 +0100

    removed unnecessary imports

--- go_zero/lib/backend/screenInfo.dart
+++ go_zero/lib/backend/screenInfo.dart
@@ -1,7 +1,3 @@
-import 'package:go_zero/screens/mvp_route/screen1.dart';
-import 'package:go_zero/screens/mvp_route/screen2.dart';
-import 'package:go_zero/screens/mvp_route/screen3.dart';
-import 'package:go_zero/screens/mvp_route/screen4.dart';
 
 //const List<Widget> screenz = [InitScreen(), ChallengeScreen(), HowItWorksScreen(), CalculateIntroScreen()];
 const int steps = 11; //screenz.length;
