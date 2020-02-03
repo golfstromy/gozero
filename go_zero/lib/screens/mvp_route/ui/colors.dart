d937ddf4b3fe7a1e3c297bb487c023674aee89a2
CodingDavid8 <codingdavid@gmx.de>
2020-02-03 15:17:45 +0100

    added default text color

--- go_zero/lib/screens/mvp_route/ui/colors.dart
+++ go_zero/lib/screens/mvp_route/ui/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 abstract class GoZeroColors {
   static const Color background = Colors.white;
   static const Color controlBackground = Colors.white;
+  static const Color defaultText = Colors.black;
 
   static const Color green = Color.fromRGBO(73, 162, 104, 1.0);
   static const Color yellow = Color.fromRGBO(235, 169, 69, 1.0);
