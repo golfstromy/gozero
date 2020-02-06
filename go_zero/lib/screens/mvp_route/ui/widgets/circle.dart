9b799a45b0b184bcd56329f78f7704f26517873f
CodingDavid8 <codingdavid@gmx.de>
2020-02-06 01:52:21 +0100

    added smallCircle widget, changed signatures

--- go_zero/lib/screens/mvp_route/ui/widgets/circle.dart
+++ go_zero/lib/screens/mvp_route/ui/widgets/circle.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_zero/screens/mvp_route/ui/screensize.dart';
 
 import '../colors.dart';
 import '../textstyle.dart';
 
-const double DEFAULTWIDTH = 264;
+const double DEFAULTWIDTHFACTOR =
+    264 / 375; //Verhältnis im Mockup Kreis:Bildschirmbreite
 
-//TODO: Width von Screengröße abhängig machen, ebenso images darin etc.
-Widget bigCircle(
-    {double size = DEFAULTWIDTH,
+//TODO: Images dadrin auch anpassen an Größe
+Widget bigCircle(BuildContext context,
+    {double sizeFactor = DEFAULTWIDTHFACTOR,
     Widget child = const SizedBox(),
-    double thickness = DEFAULTWIDTH / 132}) {
+    double thickness = 2}) {
   return Container(
-      width: size,
-      height: size,
+      width: sizeFactor * getScreenWidth(context),
+      height: sizeFactor * getScreenWidth(context),
       child: Align(alignment: Alignment.center, child: child),
       decoration: BoxDecoration(
           shape: BoxShape.circle,
@@ -21,12 +23,12 @@ Widget bigCircle(
 }
 
 const double _LOGOTEXTSIZE = 24;
-Widget circleWithText(String blacktext, String greenText,
+Widget circleWithText(BuildContext context, String blacktext, String greenText,
     {bool blackFullStop = true,
-    double size = DEFAULTWIDTH,
-    double thickness = DEFAULTWIDTH / 132}) {
-  return bigCircle(
-      size: size,
+    double sizeFactor = DEFAULTWIDTHFACTOR,
+    double thickness = 2}) {
+  return bigCircle(context,
+      sizeFactor: sizeFactor,
       thickness: thickness,
       child: RichText(
           textAlign: TextAlign.center,
@@ -36,8 +38,40 @@ Widget circleWithText(String blacktext, String greenText,
               TextSpan(text: blacktext),
               TextSpan(
                   text: greenText,
-                  style: GoZeroTextStyles.semibold( _LOGOTEXTSIZE, color: GoZeroColors.green)),
+                  style: GoZeroTextStyles.semibold(_LOGOTEXTSIZE,
+                      color: GoZeroColors.green)),
               TextSpan(text: blackFullStop ? "." : ""),
             ],
           )));
+}
+
+const double _SMALLCIRCLEBIGFONTSIZE = 30;
+const double _SMALLCIRCLESMALLFONTSIZE = 18;
+
+class SmallCircle extends StatelessWidget {
+  final double number;
+  final String unit;
+
+  const SmallCircle({@required this.number, this.unit});
+
+  @override
+  Widget build(BuildContext context) {
+    return bigCircle(context,
+        sizeFactor: 0.296,
+        child: Center(
+          child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: GoZeroTextStyles.semibold(
+                    _SMALLCIRCLEBIGFONTSIZE), //TODO: Ersetzen
+                children: <TextSpan>[
+                  TextSpan(text: number.toString()),
+                  TextSpan(
+                      text: " " + unit,
+                      style:
+                          GoZeroTextStyles.semibold(_SMALLCIRCLESMALLFONTSIZE)),
+                ],
+              )),
+        ));
+  }
 }
