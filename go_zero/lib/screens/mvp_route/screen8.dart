6cae0ae265d9f398534b829fa3a021044095cdce
CodingDavid8 <codingdavid@gmx.de>
2020-02-06 15:40:05 +0100

    wrote colorfulCircle basic structure

--- go_zero/lib/screens/mvp_route/screen8.dart
+++ go_zero/lib/screens/mvp_route/screen8.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_zero/screens/mvp_route/ui/screensize.dart';
 import 'package:go_zero/screens/mvp_route/ui/textstyle.dart';
 import 'package:go_zero/screens/mvp_route/ui/widgets/pressAnywhereLabel.dart';
 
 import 'ui/colors.dart';
 import 'ui/widgets/circle.dart';
+import 'ui/widgets/colorfulCircle.dart';
 import 'ui/widgets/headline.dart';
 
 class FootPrintResultScreen extends StatefulWidget {
@@ -16,6 +18,7 @@ double _number = 13.3;
 double _avgGermany = 8.6;
 double _avgWorld = 4.8;
 
+const double _YHEIGHTFACTORBIGCIRCLE = 0.389;
 const String NEXTSCREEN = "/screen9";
 const double _SMALLTEXTFONTSIZE = 16;
 
@@ -31,15 +34,24 @@ class _FootPrintResultScreenState extends State<FootPrintResultScreen> {
               children: <Widget>[
                 Container(color: GoZeroColors.background),
                 Headline("YOUR FOOTPRINT"),
-                colorfulCircle(_number, unit: "t per year", colors: [
-                  GoZeroColors.green,
-                  Colors.yellow,
-                  Colors.red
-                ], icons: [
-                  FontAwesomeIcons.rocket,
-                  Icons.home,
-                  Icons.person_outline
-                ]),
+                Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                        padding: EdgeInsets.only(
+                            top: _YHEIGHTFACTORBIGCIRCLE *
+                                getScreenHeight(context)),
+                        child: ColorfulCircle(_number,
+                            unit: "t per year",
+                            colors: [
+                              GoZeroColors.green,
+                              Colors.yellow,
+                              Colors.red
+                            ],
+                            icons: [
+                              FontAwesomeIcons.rocket,
+                              Icons.home,
+                              Icons.person_outline
+                            ]))),
                 Column(
                   children: <Widget>[
                     SmallCircle(number: _avgGermany, unit: "t/year"),
@@ -65,12 +77,4 @@ class _FootPrintResultScreenState extends State<FootPrintResultScreen> {
               ],
             )));
   }
-}
-
-Widget colorfulCircle(double number,
-    {unit = "",
-    List<Color> colors = const [GoZeroColors.green],
-    List<IconData> icons: const []}) {
-  //TODO: ColorfulCircle
-  return Text("Lorem ipsum");
 }
