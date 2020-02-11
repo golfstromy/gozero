06d844240abb0220ea913e22ee74af3f1c3f5721
CodingDavid8 <codingdavid@gmx.de>
2020-02-11 22:23:35 +0100

    fully implemented questions

--- go_zero/lib/screens/mvp_route/screen7.dart
+++ go_zero/lib/screens/mvp_route/screen7.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_zero/screens/mvp_route/ui/widgets/user_input/yesNoAnswer.dart';
 
 import '../../backend/screenInfo.dart';
 
@@ -28,31 +29,33 @@ const double _TITLEFONTSIZE = QuestionScreen.TITLEFONTSIZE;
 const double _GAPBETWEENCARDSFACTOR = QuestionScreen.GAPBETWEENCARDS;
 //MARGIN:SCREENWIDTH px in Mockup
 
-const double _PEOPLEHOUSEHOLDMIN = 1;
-const double _PEOPLEHOUSEHOLDMAX = 20;
-const double _PEOPLEHOUSEHOLDINIT = 3;
+const double _EUROPEFLIGHTSMIN = 0;
+const double _EUROPEFLIGHTSMAX = 100;
+const double _EUROPEFLIGHTSINIT = 50;
 
-const double _LIVINGSPACEMIN = 7;
-const double _LIVINGSPACEMAX = 999;
-const double _LIVINGSPACEINIT = 70;
+const double _INTERNATIONALFLIGHTMIN = 0;
+const double _INTERNATIONALFLIGHTMAX = 200;
+const double _INTERNATIONALFLIGHTINIT = 100;
 
-double _dHousehold = _PEOPLEHOUSEHOLDINIT;
-double _dLivingSpace = _LIVINGSPACEINIT;
-TextEditingController _householdController = TextEditingController();
-TextEditingController _livingSpaceController = TextEditingController();
+double _dEurope = _EUROPEFLIGHTSINIT;
+double _dInternational = _INTERNATIONALFLIGHTINIT;
+TextEditingController _europeFlightController = TextEditingController();
+TextEditingController _internationalFlightController = TextEditingController();
 
 class _MobilityScreenState extends State<MobilityScreen> {
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _householdController.text = _PEOPLEHOUSEHOLDINIT.round().toString();
-      _livingSpaceController.text = _LIVINGSPACEINIT.round().toString();
+      _europeFlightController.text = _EUROPEFLIGHTSINIT.round().toString();
+      _internationalFlightController.text =
+          _INTERNATIONALFLIGHTINIT.round().toString();
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    //TODO: REALIGN QUESTIONS TO FIT SCREEN COMPLETELY
     List<Widget> question1 = question(
       context,
       topPadding: 0.048 * getScreenHeight(context),
@@ -60,83 +63,68 @@ class _MobilityScreenState extends State<MobilityScreen> {
       subheadlineIcon: ICON,
       possibleAnswers: Padding(
           padding: EdgeInsets.only(top: 0.038 * getScreenHeight(context)),
-          child: SliderWithTextInput(
-              controller: _householdController,
-              min: _PEOPLEHOUSEHOLDMIN,
-              max: _PEOPLEHOUSEHOLDMAX,
-              onChangedText: (newVal) {
-                setState(() {
-                  _dHousehold = int.parse(newVal) <= _PEOPLEHOUSEHOLDMAX
-                      ? int.parse(newVal).toDouble()
-                      : _PEOPLEHOUSEHOLDMAX;
-                });
-              },
-              onChangedSlider: (value) {
-                setState(() {
-                  _dHousehold = value;
-                  _householdController.text = value.round().toString();
-                });
-              },
-              val: _dHousehold)),
+          child: YesNoAnswer()),
     );
 
     var question2 = question(
       context,
       topPadding: 0.045 * getScreenHeight(context),
-      subheadlineText: "Living space in m²",
+      subheadlineText: "Do you use public transportation?",
       subheadlineIcon: ICON,
-      possibleAnswers: Padding(
-          padding: EdgeInsets.only(top: 0.038 * getScreenHeight(context)),
-          child: SliderWithTextInput(
-              controller: _livingSpaceController,
-              min: _LIVINGSPACEMIN,
-              max: _LIVINGSPACEMAX,
-              onChangedText: (newVal) {
-                setState(() {
-                  _dLivingSpace = int.parse(newVal) <= _LIVINGSPACEMAX
-                      ? int.parse(newVal).toDouble()
-                      : _LIVINGSPACEMAX;
-                });
-              },
-              onChangedSlider: (value) {
-                setState(() {
-                  _dLivingSpace = value;
-                  _livingSpaceController.text = value.round().toString();
-                });
-              },
-              val: _dLivingSpace)),
-    );
-
-    var question3 = question(
-      context,
-      topPadding: 0.045 * getScreenHeight(context),
-      subheadlineText: "Year of construction/standard of the house",
-      subheadlineIcon: ICON,
-      possibleAnswers: SelectableSmallCardGrid(
-        //TODO: Text richtig in eine Zeile
-        crossAxisCount: 3,
-        cardWidthFactor: 89 / 375,
+      possibleAnswers: YesNoAnswer(
         padding: EdgeInsets.only(
-            top: 0.035 * getScreenHeight(context),
-            left: 0.12 * getScreenWidth(context)),
-        innerSpacing: EdgeInsets.only(
-            top: _GAPBETWEENCARDSFACTOR * getScreenHeight(context),
-            right: 12 / 375 * getScreenWidth(context)),
-        texts: [
-          "- 1978",
-          "1979 - 1983",
-          "1984 - 1994",
-          "1995 - 2001",
-          "2001+",
-          "- 1919*",
-          "1920 - 1948*",
-          "1949 - 2001*",
-          "unknown"
-        ],
-        checked: ["- 1978"],
-        //TODO: Sternchen Erklärung
+            top: 22 / 667 * getScreenHeight(context),
+            left: 45 / 375 * getScreenWidth(context)),
       ),
     );
+
+    var question3 = question(context,
+        topPadding: 0.045 * getScreenHeight(context),
+        subheadlineText: "Flights in Europe in h per year",
+        subheadlineIcon: FontAwesomeIcons.plane,
+        possibleAnswers: SliderWithTextInput(
+            padding: EdgeInsets.only(top: 25 / 667 * getScreenHeight(context)),
+            controller: _europeFlightController,
+            min: _EUROPEFLIGHTSMIN,
+            max: _EUROPEFLIGHTSMAX,
+            onChangedText: (newVal) {
+              setState(() {
+                _dEurope = int.parse(newVal) <= _EUROPEFLIGHTSMAX
+                    ? int.parse(newVal).toDouble()
+                    : _EUROPEFLIGHTSMAX;
+              });
+            },
+            onChangedSlider: (value) {
+              setState(() {
+                _dEurope = value;
+                _europeFlightController.text = value.round().toString();
+              });
+            },
+            val: _dEurope));
+
+    var question4 = question(context,
+        topPadding: 0.04 * getScreenHeight(context),
+        subheadlineText: "International flights in h per year",
+        subheadlineIcon: FontAwesomeIcons.plane,
+        possibleAnswers: SliderWithTextInput(
+            padding: EdgeInsets.only(top: 25 / 667 * getScreenHeight(context)),
+            controller: _internationalFlightController,
+            min: _INTERNATIONALFLIGHTMIN,
+            max: _INTERNATIONALFLIGHTMAX,
+            onChangedText: (newVal) {
+              setState(() {
+                _dInternational = int.parse(newVal) <= _INTERNATIONALFLIGHTMAX
+                    ? int.parse(newVal).toDouble()
+                    : _INTERNATIONALFLIGHTMAX;
+              });
+            },
+            onChangedSlider: (value) {
+              setState(() {
+                _dInternational = value;
+                _internationalFlightController.text = value.round().toString();
+              });
+            },
+            val: _dInternational));
 
     return Scaffold(
         body: SingleChildScrollView(
@@ -160,12 +148,14 @@ class _MobilityScreenState extends State<MobilityScreen> {
                             top: 0.087 * getScreenHeight(context)),
                         child: Text(_TITLE,
                             style: GoZeroTextStyles.semibold(_TITLEFONTSIZE))),
-                    question1[0],
-                    question1[1],
+                    question1[0], //QUESTION/APP OUTPUT
+                    question1[1], //ANSWER/USER INPUT
                     question2[0],
                     question2[1],
                     question3[0],
-                    question3[1]
+                    question3[1],
+                    question4[0],
+                    question4[1]
                   ],
                 ),
               ),
