674ba3ccfe17f3b8e086b2e8fbdb947d7de45c96
CodingDavid8 <codingdavid@gmx.de>
2020-02-11 21:41:45 +0100

    added fixed constants for Questions

--- go_zero/lib/backend/screenInfo.dart
+++ go_zero/lib/backend/screenInfo.dart
@@ -1,3 +1,9 @@
-
 //const List<Widget> screenz = [InitScreen(), ChallengeScreen(), HowItWorksScreen(), CalculateIntroScreen()];
 const int steps = 11; //screenz.length;
+
+abstract class QuestionScreen {
+  static const double TITLEFONTSIZE = 24;
+  static const double GAPBETWEENCARDS = 16 / 667;
+  static const double SLIDERFONTSIZE = 14;
+  static const double SLIDERTEXTMARGINFCT = 27 / 375; //MARGIN:SCREENWIDTH px in Mockup
+}
