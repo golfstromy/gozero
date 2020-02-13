66765eb39edecfd91c6c7e91f7403f9a53d25f02
David <codingdavid@gmx.de>
2020-02-13 01:11:55 +0100

    Added unit-text to circle

--- go_zero/lib/screens/mvp_route/ui/widgets/colorfulCircle.dart
+++ go_zero/lib/screens/mvp_route/ui/widgets/colorfulCircle.dart
@@ -6,7 +6,8 @@ import 'circle.dart';
 import '../colors.dart';
 
 const double _DEFAULTSIZEFACTOR = DEFAULTWIDTHFACTOR;
-const double _BIGFONTSIZE = 70;
+const double _BIGFONTSIZE = 56;
+const double _SMALLFONTSIZE = 13;
 
 class ColorfulCircle extends StatefulWidget {
   final double number;
@@ -14,12 +15,14 @@ class ColorfulCircle extends StatefulWidget {
   final List<Color> colors;
   final List<IconData> icons;
   final double fontSize;
+  final double unitFontSize;
 
   ColorfulCircle(this.number,
       {this.unit = "",
       this.colors = const [GoZeroColors.green],
       this.icons = const [],
-      this.fontSize = _BIGFONTSIZE});
+      this.fontSize = _BIGFONTSIZE,
+      this.unitFontSize = _SMALLFONTSIZE});
 
   @override
   _ColorfulCircleState createState() => _ColorfulCircleState();
@@ -28,17 +31,43 @@ class ColorfulCircle extends StatefulWidget {
 class _ColorfulCircleState extends State<ColorfulCircle> {
   @override
   Widget build(BuildContext context) {
+    //TODO: Add colorfullness
     return Container(
         width: _DEFAULTSIZEFACTOR * getScreenWidth(context),
         height: _DEFAULTSIZEFACTOR * getScreenWidth(context),
-        alignment: Alignment.center,
         decoration: BoxDecoration(
             shape: BoxShape.circle,
             border: Border.all(color: GoZeroColors.controlGrey, width: 2)),
-        child: RichText(
-            text: TextSpan(
-                style: GoZeroTextStyles.bold(fontSize: widget.fontSize),
-                text: widget.number.toString().replaceAll(".", ","),
-                )));
+        child: Stack(
+          children: <Widget>[
+            Align(
+                alignment: Alignment.center,
+                child: Text(widget.number.toString().replaceAll(".", ","),
+                    style: GoZeroTextStyles.semibold(widget.fontSize))),
+            Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      right: 16 / 375 * getScreenWidth(context),
+                      top: 25 / 667 * getScreenHeight(context), //TODO: Only estimated value, fix it properly
+                      //bottom: 99 / 667 * getScreenHeight(context)
+                      ),
+                  child: Text(widget.unit,
+                      textAlign: TextAlign.right,
+                      style: GoZeroTextStyles.semibold(widget.unitFontSize)),
+                )),
+          ],
+        ));
   }
 }
+
+/*
+RichText(textAlign: TextAlign.left,
+            text: TextSpan(
+          style: GoZeroTextStyles.semibold(widget.fontSize),
+          children: <TextSpan>[
+            TextSpan(text: widget.number.toString().replaceAll(".", ",")),
+            TextSpan(
+                text: "  " + widget.unit, style: GoZeroTextStyles.semibold(widget.unitFontSize)),
+          ],
+        ))*/
