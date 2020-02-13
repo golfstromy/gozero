9c174a43f6620fc13a6073e05862a6b94da240c7
David <codingdavid@gmx.de>
2020-02-13 01:14:23 +0100

    wrote base for congratulations screen

---
+++ go_zero/lib/screens/mvp_route/screen11.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:go_zero/screens/mvp_route/ui/widgets/pressAnywhereLabel.dart';
+
+import 'ui/colors.dart';
+
+const int INDEX = 9;
+const String NEXTSCREEN = "/screen12";
+
+class CongratulationsScreen extends StatefulWidget {
+  @override
+  _CongratulationsScreenState createState() => _CongratulationsScreenState();
+}
+
+class _CongratulationsScreenState extends State<CongratulationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: GoZeroColors.green,
+        body: GestureDetector(
+          onTap: () {
+              Navigator.pushNamed(context, NEXTSCREEN);
+            },
+            child: Container(
+          child: Stack(children: <Widget>[
+            
+            pressAnywhere(context, color: Colors.white),
+          ]),
+        )));
+  }
+}
