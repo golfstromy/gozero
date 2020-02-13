281d7dafc44b6691d8c2c775aaa8f16a4000163e
David <codingdavid@gmx.de>
2020-02-13 01:12:28 +0100

    added custom padding to circle

--- go_zero/lib/screens/mvp_route/ui/widgets/circleCaption.dart
+++ go_zero/lib/screens/mvp_route/ui/widgets/circleCaption.dart
@@ -9,8 +9,10 @@ class SmallCircleWithCaption extends StatefulWidget {
   final double number;
   final String unit;
   final String caption;
+  final EdgeInsets padding;
 
-  SmallCircleWithCaption(this.number, {this.unit, this.caption});
+  SmallCircleWithCaption(this.number,
+      {this.unit, this.caption, this.padding = const EdgeInsets.all(0)});
 
   @override
   _SmallCircleWithCaptionState createState() => _SmallCircleWithCaptionState();
@@ -19,14 +21,16 @@ class SmallCircleWithCaption extends StatefulWidget {
 class _SmallCircleWithCaptionState extends State<SmallCircleWithCaption> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SmallCircle(number: widget.number, unit: widget.unit),
-        Padding(
-            padding: EdgeInsets.only(top: 5), //TODO: Padding relativieren
-            child: Text(widget.caption,
-                style: GoZeroTextStyles.regular(_SMALLTEXTFONTSIZE)))
-      ],
-    );
+    return Container(
+        padding: widget.padding,
+        child: Column(
+          children: <Widget>[
+            SmallCircle(number: widget.number, unit: widget.unit),
+            Padding(
+                padding: EdgeInsets.only(top: 5), //TODO: Padding relativieren
+                child: Text(widget.caption,
+                    style: GoZeroTextStyles.regular(_SMALLTEXTFONTSIZE)))
+          ],
+        ));
   }
 }
